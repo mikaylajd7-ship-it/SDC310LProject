67915dd202bdf9f5c0cbe212527e3e201f023638
Mikayla Dickerson <mikaylajd7@gmail.com>
2026-08-18 22:50:14 -0400

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,18 @@
+</main>
+
+<footer class="site-footer">
+
+    <div class="container">
+
+        <p>
+            &copy; <?= date("Y") ?> Mikayla Shop.
+            SDC310L PHP Web Application Project.
+        </p>
+
+    </div>
+
+</footer>
+
+</body>
+
+</html>
